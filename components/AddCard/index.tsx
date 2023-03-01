ad33cd0d1657628bede0c1c1877fee84e0ba8cc9
Lara Mateo <asdasd5056@gmail.com>
2023-03-01 18:04:09 -0300

    AddCart

--- components/AddCard/index.tsx
+++ components/AddCard/index.tsx
@@ -1,34 +1,37 @@
-'use client'
+'use client';
 import styles from './styles.module.css';
+import { useRouter } from 'next/navigation';
 
 const AddCard = () => {
-
-  const newPost = (e) => {
+  const router = useRouter();
+  const newPost = async (e) => {
     e.preventDefault();
-    fetch ('/api/post', {
+    await fetch('/api/post', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        content : e.target.content.value,
-        author : e.target.author.value,
+        content: e.target.content.value,
+        author: e.target.author.value,
       }),
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+    });
+
+    router.refresh();
   };
 
   return (
     <section className={styles.addCard}>
       <h3>Write a message to someone who needs it.</h3>
-      <form  onSubmit={newPost}>
+      <form onSubmit={newPost}>
         <label htmlFor='name'>From</label>
-        <input name="author" type='text' placeholder='Your name' />
+        <input name='author' type='text' placeholder='Your name' />
         <label htmlFor='name'>To</label>
-      <textarea placeholder='I hope you achieve your dreams'  name="content"></textarea>
-      <input type='submit' value='Submit' />
-        </form>
+        <textarea
+          name='content'
+          placeholder='I hope you achieve your dreams'></textarea>
+        <input type='submit' value='Submit' />
+      </form>
     </section>
   );
 };
