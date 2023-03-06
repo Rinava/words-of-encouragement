cf79e3cf1efa29b5090dd448e2b506c4a616c2cd
Lara Mateo <asdasd5056@gmail.com>
2023-03-06 17:06:26 -0300

    lint

--- components/AddCard/index.tsx
+++ components/AddCard/index.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 const AddCard = () => {
   const router = useRouter();
-  const newPost = async (e) => {
+  const newPost = async (e: any) => {
     e.preventDefault();
     await fetch('/api/post', {
       method: 'POST',
@@ -22,13 +22,13 @@ const AddCard = () => {
   };
   const heart = {
     hidden: { scale: 0 },
-    hover: { scale: 1.4, transition: { duration: 0.3  } },
+    hover: { scale: 1.4, transition: { duration: 0.3 } },
     tap: { scale: 1, transition: { duration: 0.6 } },
   };
 
   const button = {
-    hidden: { scale: 1 , opacity: 0.8},
-    hover: { scale: 1.1, opacity: 1},
+    hidden: { scale: 1, opacity: 0.8 },
+    hover: { scale: 1.1, opacity: 1 },
     tap: { scale: 0.5, transition: { duration: 0.5 } },
   };
 
@@ -65,11 +65,11 @@ const AddCard = () => {
           whileHover='hover'
           whileTap='tap'>
           <motion.div className={styles.heart} variants={heart}>
-          ❤︎
+            ❤︎
           </motion.div>
           Send
           <motion.div className={styles.heart} variants={heart}>
-          ❤︎
+            ❤︎
           </motion.div>
         </motion.button>
       </form>
